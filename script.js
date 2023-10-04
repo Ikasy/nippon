e502f1c81e6d1bdace0b11ae685a5299c65a2634
JakobYdeNaerum <126673163+JakobYdeNaerum@users.noreply.github.com>
2023-10-04 14:11:58 +0200

    Signup/loginsiden

--- script.js
+++ script.js
@@ -68,3 +68,19 @@ function openDropdown(){
         omlist.style.display = "block";
     }
 }
+// login og signup siden
+
+let signup = document.querySelector(".signup");
+let login = document.querySelector(".login");
+let slider = document.querySelector(".slider");
+let formSection = document.querySelector(".form-section");
+
+signup.addEventListener("click", () => {
+	slider.classList.add("moveslider");
+	formSection.classList.add("form-section-move");
+});
+
+login.addEventListener("click", () => {
+	slider.classList.remove("moveslider");
+	formSection.classList.remove("form-section-move");
+});
